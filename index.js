369a22fe8e398de664561360dbf7c5290a008da3
Jahirul Islam <jahirulislam@Shafiqs-MacBook-Pro-2.local>
2022-11-10 21:35:11 +0600

    My reviews Page added

--- index.js
+++ index.js
@@ -34,6 +34,12 @@ async function run(){
     try{
         const serviceCollection = client.db('goTravel').collection('services');
 
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '2h'})
+            res.send({token});
+        })
+
         app.get('/services', async(req, res) => {
             const query = {}
             const cursor = serviceCollection.find(query);
