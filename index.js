08b5074b0be83878eb870f8b42077c5fb106d9fb
Jahirul Islam <jahirulislam@Shafiqs-MacBook-Pro-2.local>
2022-11-10 21:26:35 +0600

    JWT token added

--- index.js
+++ index.js
@@ -14,23 +14,31 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.lo9eyry.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next){
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).send({message: 'unauthorized access'});
+    }
+    const token = authHeader.split(' ')[1];
+    
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
+        if(err){
+            return res.status(403).send({message: 'Forbidden access'});
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 async function run(){
     try{
         const serviceCollection = client.db('goTravel').collection('services');
-        // const homeServiceCollection = client.db('goTravel').collection('homeService');
 
         app.get('/services', async(req, res) => {
             const query = {}
             const cursor = serviceCollection.find(query);
             const services = await cursor.limit(3).toArray();
             res.send(services);
-        })
-
-        app.get('/homeService', async(req, res) => {
-            const query = {}
-            const cursor = serviceCollection.find(query);
-            const homeService = await cursor.limit(3).toArray();
-            res.send(homeService);
         })
 
         app.get('/services/:id', (req, res) => {
